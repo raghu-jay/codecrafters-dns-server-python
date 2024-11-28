4c0e843ad1aa0f6f9fb209bc9b917622baf600ea
Raghu Jayathirtha <raghuttam@Raghus-MacBook-Pro.local>
2024-11-28 20:55:41 +0530

    msg

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ def main():
 
     # Uncomment this block to pass the first stage
     #
-     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-     udp_socket.bind(("127.0.0.1", 2053))
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(("127.0.0.1", 2053))
     #
     # while True:
     #     try:
